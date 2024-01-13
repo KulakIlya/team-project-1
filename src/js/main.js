8d15fcc7fdd8655de76fc100696cd1278ac9ef9d
Ilya <kulak1224@gmail.com>
2024-01-13 20:46:47 +0200

    adding navigation functionality

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,10 @@ const menu = document.querySelector('.overlay');
 const openBtn = document.querySelector('.burger-btn');
 const closeBtn = document.querySelector('.mm-close-btn');
 
+menu?.addEventListener('click', e => {
+  if (e.target.closest('.mm-item')) toggleClasses();
+});
+
 openBtn?.addEventListener('click', toggleClasses);
 closeBtn?.addEventListener('click', toggleClasses);
 

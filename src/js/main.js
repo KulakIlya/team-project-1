5bbe1bae26bacdda46ddfbb0e5ac261cf58a5045
Ilya <kulak1224@gmail.com>
2024-01-16 10:15:51 +0200

    test color fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import 'modern-normalize';
+
 import './open-close-menu';
+
+AOS.init({
+  offset: 0,
+  delay: 50,
+  duration: 1000,
+  easing: 'ease-in-out',
+  once: 'true',
+});
